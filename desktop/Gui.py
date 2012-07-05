057730f1d08d9f9cb40f9f8fbf304737d15fb66f
Alfonso E.M <alfonso@el-magnifico.org>
2012-07-05 21:58:43 +0200

    Isolating controller

--- desktop/Gui.py
+++ desktop/Gui.py
@@ -28,20 +28,59 @@ class Interface:
             self.screen=pygame.display.set_mode((self.width,self.height),pygame.FULLSCREEN)
         else:
             self.screen=pygame.display.set_mode((self.width,self.height))
-            
+
+        self.font_big = pygame.font.Font(CONF["font_name"], int(CONF["font_size_big"]) )
         self.font = pygame.font.Font(CONF["font_name"], int(CONF["font_size"]) )
+        self.font_small = pygame.font.Font(CONF["font_name"], int(CONF["font_size_small"]) )
         self.back_color=pygame.color.Color(CONF["back_color"])
+        self.front_color_active=pygame.color.Color(CONF["front_color_active"])
         self.front_color=pygame.color.Color(CONF["front_color"])
         self.font_color=pygame.color.Color(CONF["font_color"])
 
         pygame.mouse.set_visible(0)
-        self.screen.fill(self.back_color)
 
-        text = self.font.render("TeleFam", 1, self.font_color)
+    def clean(self):
+        self.screen.fill(self.back_color)
+        pygame.display.flip()
+
+    def draw_box(self,rectangle,active):
+        if active:
+            pygame.draw.rect(self.screen, self.front_color_active, rectangle, 0)
+        else:
+            pygame.draw.rect(self.screen, self.front_color, rectangle, 0)
+
+
+    def show_main_menu(self):
+        text = self.font_big.render("TeleFam", 1, self.font_color)
         width,height=text.get_size()
         x=self.width/2-width/2
+        self.draw_box((x,10,width,height),False)
+        self.draw_box((x,100,width,height),True)
+        pygame.draw.rect(self.screen, self.front_color, (x,10,width,height), 0)
         self.screen.blit(text, (x,10))
         pygame.display.flip()
         
-        pygame.time.delay(3000)
-         
+#        pygame.time.delay(3000)
+
+    def wait_for_user(self):
+      while True:
+        for event in pygame.event.get() :
+          if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_ESCAPE:
+              sys.exit(0)
+            if event.key == pygame.K_SPACE :
+              return "OK"
+            if event.key == pygame.K_UP :
+              return "UP"
+            if event.key == pygame.K_DOWN :
+              return "DOWN"
+            if event.key == pygame.K_UP :
+              return "UP"
+            if event.key == pygame.K_DOWN :
+              return "DOWN"
+            if event.key == pygame.K_RIGH :
+              return "RIGHT"
+            if event.key == pygame.K_LEFT :
+              return "LEFT"
+
+
